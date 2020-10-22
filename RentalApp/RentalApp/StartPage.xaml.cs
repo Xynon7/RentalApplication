0f74a2dc2e3fc29612e14fb7385053b3a0de2150
Andrew Brown <abrown2358@gmail.com>
2020-10-21 19:55:48 -0500

    Made starter UI

---
+++ RentalApp/RentalApp/StartPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RentalApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewPage : ContentPage
+	{
+		public NewPage ()
+		{
+
+            InitializeComponent();
+		}
+
+        async void OnSignUpClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUpPage());
+        }
+        async void  OnLogInClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUpPage());
+        }
+    }
+}
