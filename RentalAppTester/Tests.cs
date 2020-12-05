bd04c4ab848d82bdd7d909dd4a00dc440b100cc8
crclawson <chase.clawson@flyingteegolf.com>
2020-12-04 20:01:20 -0600

    Fixing Message retrieval and adding message tests

--- RentalAppTester/Tests.cs
+++ RentalAppTester/Tests.cs
@@ -6,6 +6,8 @@ using Xamarin.UITest;
 using Xamarin.UITest.Queries;
 using RentalApp;
 using RentalsApp;
+using RentalsApp.DBObjects;
+using System.Collections.Generic;
 
 namespace RentalAppTester
 {
@@ -22,19 +24,31 @@ namespace RentalAppTester
         }
 
         [Test]
-        public void WelcomeTextIsDisplayed()
-        {
-            AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
-            app.Screenshot("Welcome screen.");
-
-            Assert.IsTrue(results.Any());
-        }
-
-        [Test]
-        public void CheckConnectionThroughInvalidLogin()
+        public void CheckInvalidLoginFunctionality()
         {
             bool isValidLogin = RASQLManager.sqlManagerInstance.ValidateLogin("", "");
             Assert.IsFalse(isValidLogin, "Login should not have been valid");
+        }
+
+        [Test]
+        public void CheckValidLoginFunctionality()
+        {
+            bool isValidLogin = RASQLManager.sqlManagerInstance.ValidateLogin("Den1S80", "Belloto9021");
+            Assert.IsTrue(isValidLogin, "Login should have been valid");
+        }
+
+        [Test]
+        public void CheckMessagesSentFunctionality()
+        {
+            List<Communication> messages = RASQLManager.sqlManagerInstance.GetMessagesSent("Jnnife016");
+            Assert.IsFalse(messages.Count == 0, "Message list should not be empty");
+        }
+
+        [Test]
+        public void CheckMessagesReceivedFunctionality()
+        {
+            List<Communication> messages = RASQLManager.sqlManagerInstance.GetMessagesReceived("Den1S80");
+            Assert.IsFalse(messages.Count == 0, "Message list should not be empty");
         }
     }
 }
