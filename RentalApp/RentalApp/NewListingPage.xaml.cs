4dec9aa8ed45f00fab1dc65c6d74327474827efd
Andrew Brown <abrown2358@gmail.com>
2020-12-04 20:35:27 -0600

    New Listings page made

--- RentalApp/RentalApp/NewListingPage.xaml.cs
+++ RentalApp/RentalApp/NewListingPage.xaml.cs
@@ -12,9 +12,38 @@ namespace RentalApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewListingPage : ContentPage
     {
+        public string desc, brand, type, cost;
         public NewListingPage()
         {
             InitializeComponent();
         }
+
+        void DescriptionCompleted(object sender, EventArgs e)
+        {
+            desc = ((Entry)sender).Text;
+        }
+
+        void BrandCompleted(object sender, EventArgs e)
+        {
+            brand = ((Entry)sender).Text;
+        }
+
+
+        void TypeCompleted(object sender, EventArgs e)
+        {
+            type = ((Entry)sender).Text;
+        }
+
+
+        void CostCompleted(object sender, EventArgs e)
+        {
+            cost = ((Entry)sender).Text;
+        }
+
+        async void OnSubmitClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NewListingPage());
+        }
+
     }
 }
